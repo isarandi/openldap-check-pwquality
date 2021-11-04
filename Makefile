7f9569769024d9553f7f65cecd936e274699c940
Istvan Sarandi <sarandi@vision.rwth-aachen.de>
2021-11-04 17:25:22 +0100

    Add sources

---
+++ Makefile
@@ -0,0 +1,21 @@
+CC=gcc
+
+# Where to find the OpenLDAP sources
+LDAP_SRC=/tmp/openldap-2.4.45+dfsg
+LDAP_LIBDIR=/usr/lib/ldap
+CONFIG_PATH=/etc/ldap/pwquality.conf
+CC_FLAGS=-g -O2 -Wall -fpic
+
+all: 	check_pwquality.so
+
+check_pwquality.o: check_pwquality.c
+	$(CC) $(CC_FLAGS) -c -I $(LDAP_SRC)/include -I $(LDAP_SRC)/servers/slapd -DCONFIG_PATH="\"$(CONFIG_PATH)\"" $<
+
+check_pwquality.so: check_pwquality.o
+	$(CC) -shared -o $@ $< -lpwquality
+
+install: check_pwquality.so
+	cp -f $< $(LDAP_LIBDIR)/
+
+clean:
+	$(RM) check_pwquality.o check_pwquality.so
---
+++ check_pwquality.c
@@ -0,0 +1,73 @@
+/*
+    check_pwquality - password policy module for OpenLDAP using libpwquality
+
+    Copyright (C) 2021 Istvan Sarandi <istvan.sarandi@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <string.h>
+#include <portable.h>
+#include <slap.h>
+#include <pwquality.h>
+#include <syslog.h>
+
+int check_password(const char *pPasswd, char **ppErrStr __attribute__((unused)), const Entry *pEntry)
+{
+    // Get pwquality settings
+    pwquality_settings_t *pwq;
+    int rv;
+    void *auxerror;
+    char buf[PWQ_MAX_ERROR_MESSAGE_LEN];
+    pwq = pwquality_default_settings();
+    if (!pwq) {
+        syslog(LOG_ERR, "Error in check_pwquality: %s\n", pwquality_strerror(buf, sizeof(buf), PWQ_ERROR_MEM_ALLOC, NULL));
+        return EXIT_FAILURE;
+    }
+    if ((rv=pwquality_read_config(pwq, CONFIG_PATH, &auxerror)) != 0) {
+        pwquality_free_settings(pwq);
+        syslog(LOG_ERR, "Error in check_pwquality: %s\n", pwquality_strerror(buf, sizeof(buf), rv, auxerror));
+        return EXIT_FAILURE;
+    }
+
+    // Get username between equals sign and comma
+    if (!pEntry)
+        return EXIT_FAILURE;
+    char *equals_ptr = strchr(pEntry->e_name.bv_val, '=');
+    if (!equals_ptr)
+        return EXIT_FAILURE;
+    char *comma_ptr = strchr(equals_ptr + 1, ',');
+    if (!comma_ptr)
+        return EXIT_FAILURE;
+    size_t namelen = comma_ptr - equals_ptr;
+    char *username = malloc(namelen + 1);
+    strncpy(username, equals_ptr + 1, namelen);
+    username[namelen] = '\0';
+
+    // Check quality
+    rv = pwquality_check(pwq, pPasswd, NULL, username, &auxerror);
+    pwquality_free_settings(pwq);
+    free(username);
+
+    if (rv >= 0) {
+        return LDAP_SUCCESS;
+    } else {
+        const char *prefix = "The password is too weak: ";
+        size_t prefix_len = strlen(prefix);
+        const char *errstr = pwquality_strerror(buf, sizeof(buf), rv, auxerror);
+        *ppErrStr = malloc(prefix_len + PWQ_MAX_ERROR_MESSAGE_LEN);
+        strncpy(*ppErrStr, prefix, prefix_len);
+        strncpy((*ppErrStr) + prefix_len, errstr, PWQ_MAX_ERROR_MESSAGE_LEN);
+        return EXIT_FAILURE;
+    }
+}
